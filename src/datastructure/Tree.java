8e75923a9c36de4178e48a6eb513ad3e4051c0ba
TheresaBultinck <theresa_bultinck@hotmail.com>
2017-04-15 14:06:43 +0200

    Made TreeNode class a nested class in Tree

--- src/datastructure/Tree.java
+++ src/datastructure/Tree.java
@@ -12,4 +12,50 @@ public class Tree<T> {
 		return root;
 	}
 	
+	//Interface: ooit een element van maken? 
+	private class TreeNode<T>{
+		
+		private TreeNode<T> parent = null; 
+		private LinkedList<TreeNode<T>> children = new LinkedList<TreeNode<T>>();
+		private T element;
+		
+		public TreeNode(T e){
+			this.element = e;
+		}
+		
+		/**
+		 * Adds a child to a linked list of children
+		 * @param child 
+		 * @return true if a parent has been added and thus also a child and 
+		 * false if it failed to add a parent and thus no child
+		 */
+		public boolean addChild(TreeNode<T> child){
+			if (child.addParent(this)){
+				children.add(child);
+				return true; 
+			}
+			return false;
+		}
+		
+		/**
+		 * Adds a parent
+		 * @param parent
+		 * @return true if there isn't yet a parent and false if there is already a parent present
+		 */
+		public boolean addParent(TreeNode<T> parent){
+			if(this.isRoot()){
+				this.parent = parent;
+				return true;
+			}
+			return false;
+		}
+		
+		/**
+		 * Check if there is a parent
+		 * @return true if there is no parent and false if there is already a parent
+		 */
+		public boolean isRoot(){
+			return parent == null;
+		}
+	}
 }

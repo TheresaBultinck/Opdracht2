16fe89ecd558361fbb69c92ec4f525c3b25006ca
TheresaBultinck <theresa_bultinck@hotmail.com>
2017-04-21 13:17:09 +0200

    Made an AbstractStatus class and implemented some basic getters and setters

--- src/program/AbstractStatus.java
+++ src/program/AbstractStatus.java
@@ -5,5 +5,29 @@ public class AbstractStatus {
 	private StatusProperty prop;
 	private int position;
 	
+	public AbstractStatus (int pos, StatusProperty prop){
+		setStatusProperty(prop);
+		this.position = pos; 
+	}
 	
+	public int getStartPosition(){
+		return position;
+	}
+	
+	public int getEndPosition(){
+		return position+2;
+	}
+	
+	public StatusProperty getStatusProperty(){
+		return prop;
+	}
+	
+	private void setStatusProperty (StatusProperty prop){
+		this.prop = prop;
+	}
+	
+	@Override
+	public String toString(){
+		return prop.toString() + " on " + position;
+	}
 }

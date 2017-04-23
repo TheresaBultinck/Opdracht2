324115cf528ecfbb7ebeaef8d77e3d2217bd4d02
TheresaBultinck <theresa_bultinck@hotmail.com>
2017-04-23 12:54:19 +0200

    Added Javadoc

--- src/program/StatusProperty.java
+++ src/program/StatusProperty.java
@@ -1,5 +1,10 @@
 package program;
 
+/**
+ * Enum class with two options: error or ok.
+ * @author Theresa Bultinck
+ *
+ */
 public enum StatusProperty {
 
 	ERROR(), OK();
--- src/program/Tag.java
+++ src/program/Tag.java
@@ -1,5 +1,9 @@
 package program;
 
+/**
+ * Defines a tag between two characters ('<' and '>').
+ * @author Theresa Bultinck
+ */
 public class Tag {
 	
 	private String name;
@@ -10,42 +14,69 @@ public class Tag {
 		this.offset = offset;
 	}
 	
+	/**
+	 * Default constructor
+	 */
 	public Tag(){
 		this.offset = 0;
 	}
 	
 	/**
-	 * If it is an opening tag ('<'), isOpen is set to true and if it is an closing tag ('<\') 
+	 * If it is an opening character ('<'), isOpen is set to true and if it is an closing character ('</') 
 	 * isOpen is set to false. 
 	 */
 	public void close() {
 		isOpen = false;
 	}
 
+	/**
+	 * Change the name of the tag
+	 * @param text
+	 */
 	public void setName(String text) {
 		name = text;
 	}
 	
+	/**
+	 * @return true if the tag is open and false if the tag isn't opened
+	 */
 	public boolean isOpen() {
 		return isOpen;
 	}
 	
+	/**
+	 * @return true if the tag is closed and false if the tag is still open
+	 */
 	public boolean isClose() {
 		return !isOpen;
 	}
 	
+	/**
+	 * @return what is the name of the tag. For example "html", "body".
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * @return the length of the tag
+	 */
 	public int getLength(){
 		return this.name.length() + 2;
 	}
 	
+	/**
+	 * Position of the first character of the tag. 
+	 * @return offset
+	 */
 	public int getStartPosition(){
 		return offset; 
 	}
 	
+	/**
+	 * Position of the last character of the tag.
+	 * @return
+	 */
 	public int getEndPosition(){
 		return offset + getLength();
 	}
